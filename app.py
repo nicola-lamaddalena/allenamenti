a6cf6ab9248368b5de027eb43cdba6ec2b0c8158
Uomonero1 <niks9498@gmail.com>
2023-01-21 16:58:35 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,50 @@
+import os
+from flask import Flask, render_template, request, redirect, url_for
+from connessione import Connessione
+import matplotlib.pyplot as plt
+
+DB = "fitness.db"
+GRAFICI_DIR = os.path.join("static", "graficiAllenamenti")
+
+app = Flask(__name__)
+app.config["CARICA_DIR"] = GRAFICI_DIR
+
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        render_template("nuovoAllenamento.html")
+
+    return render_template("index.html")
+
+@app.route("/nuovoAllenamento", methods=["GET", "POST"])
+def aggiungi_allenamento():
+    if request.method == "POST":
+        esercizio = request.form.get("nome")
+        ripetizioni = int(request.form.get("ripetizioni"))
+        serie = int(request.form.get("serie"))
+        riposo = float(request.form.get("riposo"))
+        giorno = request.form.get("giorno")
+        gruppo_musc = int(request.form.get("gruppo_musc"))
+        conn = Connessione("fitness.db")
+        conn.insert(esercizio, ripetizioni, serie, riposo, giorno, gruppo_musc)
+
+    return render_template("nuovoAllenamento.html")
+
+@app.route("/visualizzaAllenamenti")
+def visualizza_allenamenti():
+    conn = Connessione("fitness.db")
+    dati = conn.display()
+    return render_template('visualizzaAllenamenti.html', dati=dati)
+
+@app.route("/grafici")
+def viz():
+    full_filename = os.path.join(app.config["CARICA_DIR"], "grafici.png")
+    conn = Connessione(DB)
+    dati = conn.display()
+
+    if "grafico.png" not in app.config["CARICA_DIR"]:
+        plt.bar([dato[4] for dato in dati], [dato[1] for dato in dati])
+        plt.savefig(full_filename)
+
+    return render_template("grafici.html", grafico = full_filename)
---
+++ connessione.py
@@ -0,0 +1,77 @@
+import sqlite3
+from datetime import datetime
+
+class Connessione:
+    def __init__(self, db):
+        self.conn = sqlite3.connect(db)
+        self.cursor = self.conn.cursor()
+
+        self.cursor.execute("""
+        CREATE TABLE IF NOT EXISTS esercizi (
+            id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE NOT NULL,
+            nome TEXT (50) NOT NULL,
+            numero_ripetizioni INTEGER NOT NULL,
+            numero_serie INTEGER NOT NULL,
+            riposo_minuti NUMERIC NOT NULL,
+            giorno DATE NOT NULL,
+            gruppo_muscolare INTEGER REFERENCES [gruppo_muscolare] (id) ON DELETE SET NULL ON UPDATE SET NULL
+            );
+        """)
+        self.conn.commit()
+
+        self.cursor.execute("""
+        CREATE TABLE IF NOT EXISTS gruppo_muscolare (
+            id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL UNIQUE,
+            nome TEXT (50) NOT NULL UNIQUE ON CONFLICT IGNORE
+            );
+        """)
+        self.conn.commit()
+
+        self.cursor.execute("""
+            INSERT OR IGNORE INTO gruppo_muscolare(nome)
+            VALUES
+                ("Pettorali"),
+                ("Addominali"),
+                ("Bicipiti"),
+                ("Deltoidi"),
+                ("Tricipiti"),
+                ("Dorsali"),
+                ("Trapezi"),
+                ("Quadricipiti"),
+                ("Polpacci")
+            ;
+        """)
+        self.conn.commit()
+
+    def insert(self,
+                            nome: str,
+                            numero_ripetizioni: int,
+                            numero_serie: int,
+                            riposo_minuti: float,
+                            giorno: datetime,
+                            gruppo_muscolare: int):
+        self.cursor.execute("""
+            INSERT INTO esercizi(nome, numero_ripetizioni, numero_serie, riposo_minuti, giorno, gruppo_muscolare)
+            VALUES(?,?,?,?,?,?);
+        """, 
+        (nome, numero_ripetizioni, numero_serie, riposo_minuti, giorno, gruppo_muscolare)
+        )
+        self.conn.commit()
+
+    def display(self):
+        self.cursor.execute("""
+            SELECT
+                esercizi.id,
+                esercizi.nome, 
+                numero_ripetizioni, 
+                numero_serie, 
+                riposo_minuti, 
+                giorno, 
+                gruppo_muscolare.nome AS gruppo_muscolare
+            FROM esercizi
+            JOIN gruppo_muscolare
+            ON esercizi.gruppo_muscolare = gruppo_muscolare.id;
+            """)
+        return self.cursor.fetchall()
+
+    
